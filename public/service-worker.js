1e845733264b43cd151b893a9b817601c9ff6fe5
Tifanny Rivera <trivera51580@gmail.com>
2021-11-15 12:41:31 -0700

    still working service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,18 +1,16 @@
-console.log('Hello from your service worker');
-
 const CACHE_NAME = "static-cache-v2";
 const RUNTIME = 'runtime';
 
 const FILES_TO_CACHE = [
     '/',
+    '/db.js',
     '/index.html',
     '/index.js',
     '/styles.css',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png',
-    '/manifest.webmanifest'
-    // "/dist/bundle.js",
-    // "/dist/manifest.webmanifest"
+    '/manifest.webmanifest',
+    '/service-worker.js'
   ];
 
   // INSTALL
