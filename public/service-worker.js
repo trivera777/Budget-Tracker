a80d38692c80f5a929fa346ef1aedcc37f368604
Tifanny Rivera <trivera51580@gmail.com>
2021-11-26 20:51:22 -0700

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,10 @@
+// register
+// install
+// activate
+// fetch
+
 const CACHE_NAME = "static-cache-v2";
-const RUNTIME = 'runtime';
+const DATA_CACHE_NAME = "data-cache-v1";
 
 const FILES_TO_CACHE = [
     '/',
@@ -9,7 +14,7 @@ const FILES_TO_CACHE = [
     '/styles.css',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png',
-    '/manifest.webmanifest',
+    '/manifest.json',
     '/service-worker.js'
   ];
 
@@ -23,6 +28,7 @@ const FILES_TO_CACHE = [
             return cache.addAll(FILES_TO_CACHE)
         })
     )
+    self.skipWaiting();
   });
 
 // activate 
@@ -39,4 +45,41 @@ self.addEventListener('activate', (event) => {
            }))
        })
     )
+    self.clients.claim();
+})
+
+self.addEventListener('fetch', (event) => {
+  if(event.request.url.includes('/api')){
+    console.log('fetching...');
+    event.respondWith(
+      caches.open(DATA_CACHE_NAME)
+      .then(cache => {
+        return fetch(event.request)
+        .then(response => {
+          if(response.status === 200){
+            cache.put(event.request.url, response.clone())
+          }
+          return response;
+        })
+        .catch(error => {
+          return cache.match(event.request)
+        })
+      }).catch(error => console.log(error))
+    )
+    return
+  }
+
+  event.respondWith(
+    fetch(event.request)
+    .catch(function() {
+      return caches.match(event.request)
+      .then((response) => {
+        if(response){
+          return response
+        } else if (event.request.headers.get('accept').includes('text/html')){
+          return caches.match('/')
+        }
+      })
+    })
+  )
 })
